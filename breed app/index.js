6ee49b9197ee389fd274f2155df8d36778bd43ce
Joey Romo <joeyjr95@gmail.com>
2019-08-22 12:51:09 -0400

    almost finished 3rd part

---
+++ breed app/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+
+function getDogBreed(breed) {
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => displayImage(responseJson))
+    .catch(error =>alert('nope'));
+}
+
+function handleImageRequest() {
+  $('.js-number-entry-submit').click( e => {
+    
+    $('.js-dog-gallery').html('');
+    e.preventDefault();
+    const breed = $('.js-number-images-entry').val();
+    getDogBreed(breed);
+  });
+}
+
+function displayImage(data) {
+  $('.js-dog-gallery').html(`<img src='${data.message}' alt='dog'></img>`);
+}
+
+
+function main() {
+  handleImageRequest();
+}
+
+$(main);
